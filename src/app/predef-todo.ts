3cf93965f38125252e0e2aa9f4b9b853ef511d64
Harsh kumar Sharma <hk.harshkumar11@gmail.com>
2020-10-15 22:40:51 +0530

    first commit

---
+++ src/app/predef-todo.ts
@@ -0,0 +1,7 @@
+import {Todo} from './todo';
+export const Todos: Todo[] = [
+{ id : 1, date : '12/1/2020', title : 'yoga', description : 'to do yoga' },
+{ id : 2, date : '12/2/2020', title : 'homework', description : 'to do homework'},
+{ id : 3, date : '12/3/2020', title : 'watering plants', description : 'to water plants'},
+{ id : 4, date : '12/4/2020', title : 'gym', description : 'to do gym'}
+];
